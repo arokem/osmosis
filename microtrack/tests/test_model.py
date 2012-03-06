BF + TST: Fixed the calculation of coeff of determination.

microtrack/tests/test_model.py
@@ -31,6 +31,9 @@ def test_BaseModel():
 
     BM = mtm.BaseModel(DWI)
     npt.assert_equal(BM.r_squared, np.ones(BM.signal.shape[:3]))
+    npt.assert_equal(BM.R_squared, np.ones(BM.signal.shape[:3]))
+    npt.assert_equal(BM.coeff_of_determination, np.ones(BM.signal.shape[:3]))
+    
     
 @npt.decorators.slow
 @npt.decorators.skipif(no_data)
@@ -80,5 +83,6 @@ def test_SphericalHarmonicsModel():
     """
     Test the estimation of SH models.
     """
-    
+
+    pass
     

microtrack/utils.py
@@ -380,10 +380,9 @@ def explained_variance(data, model, axis=-1):
 
     """
     residuals = data - model
-    1/0.
     ss_err = np.sum(residuals ** 2, axis=axis)
 
-    demeaned_data = data - np.mean(data)
+    demeaned_data = data - np.mean(data,-1)[...,np.newaxis]
     ss_tot = np.sum(demeaned_data **2, axis=axis)
 
     return (1 - (ss_err/ss_tot))