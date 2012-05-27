8d86ff7b6448d1282684d68c97ed2fad5c751fdf
arokem <arokem@gmail.com>
2012-05-27 08:51:18 -0700

    RF: Tune Lasso sparsity constraint.

--- osmosis/model.py
+++ osmosis/model.py
@@ -3149,7 +3149,7 @@ class SparseDeconvolutionModel(CanonicalTensorModel):
             for vox in xrange(self._flat_signal.shape[0]):
                 # Fit the deviations from the mean of the fitted signal: 
                 sig = fit_to.T[vox] - np.mean(fit_to.T[vox])
-                solver = Lasso()
+                solver = Lasso(alpha=0.01)
                 params[vox] = solver.fit(design_matrix, sig).coef_
                 if self.verbose:
                     prog_bar.animate(vox, f_name=f_name)
