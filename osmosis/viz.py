d8b7e94f23a7fec8855dd6113bd65384b3c1b105
arokem <arokem@gmail.com>
2012-08-21 15:40:17 -0700

    NF: Added visualization of data on cut-planes into a 3d anatomy.

--- osmosis/viz.py
+++ osmosis/viz.py
@@ -16,7 +16,8 @@ except ImportError:
 
 try:
     from mayavi import mlab as maya
-    from mayavi.api import Engine
+    import mayavi.tools as maya_tools
+    EM = maya_tools.engine_manager.EngineManager()
     have_maya = True
 except ImportError:
     have_maya = False
@@ -685,17 +686,21 @@ def plot_ellipsoid_mpl(Tensor, n=60):
     return fig
 
 
-def _display_maya(x_plot, y_plot, z_plot, faces, scalars, cmap='jet',
+def _display_maya_voxel(x_plot, y_plot, z_plot, faces, scalars, cmap='jet',
                    colorbar=False, figure=None, vmin=None, vmax=None,
                    file_name=None):
-    
-    if figure is None: 
-        engine = Engine()
-        engine.start()
+
+    if figure is None:
         # Adding the engine to a figure will subsequently give you control
         # through the mayavi GUI:
+        engine = EM.new_engine()
         figure = maya.figure(engine)    
-    
+    else:
+        figure, engine = figure
+
+    if len(engine.scenes)==0:
+        engine.new_scene()
+
     tm = maya.triangular_mesh(x_plot, y_plot, z_plot, faces, scalars=scalars,
                               colormap=cmap, figure=figure)
     if colorbar:
@@ -709,9 +714,9 @@ def _display_maya(x_plot, y_plot, z_plot, faces, scalars, cmap='jet',
 
     # Take care of the color-map:
     if vmin is None:
-        vmin = np.min(v)
+        vmin = np.min(scalars)
     if vmax is None:
-        vmax = np.max(v)
+        vmax = np.max(scalars)
 
     scene.parallel_projection=True
     
@@ -728,12 +733,12 @@ def _display_maya(x_plot, y_plot, z_plot, faces, scalars, cmap='jet',
     scene.render()
     if file_name is not None:
         scene.save(file_name)
-    engine.stop()
-    return figure
+
+    return figure,engine
 
 
 def plot_tensor_3d(Tensor, cmap='jet', mode='ADC', file_name=None,
-                        colorbar=False, figure=None, vmin=None, vmax=None):
+                   colorbar=False, figure=None, vmin=None, vmax=None, offset=0):
 
     """
 
@@ -765,14 +770,15 @@ def plot_tensor_3d(Tensor, cmap='jet', mode='ADC', file_name=None,
     x_plot, y_plot, z_plot = geo.sphere2cart(v, phi, theta)
 
     # Call and return straightaway:
-    return _display_maya(x_plot, y_plot, z_plot, faces, v, cmap=cmap,
-                         colorbar=colorbar, figure=None, vmin=vmin, vmax=vmax,
+    return _display_maya_voxel(x_plot, y_plot, z_plot+offset, faces, v, cmap=cmap,
+                         colorbar=colorbar, figure=figure, vmin=vmin, vmax=vmax,
                          file_name=file_name)
     
 
 
 def plot_signal_interp(signal, bvecs, cmap='jet', file_name=None,
-                        colorbar=False, figure=None, vmin=None, vmax=None):
+                        colorbar=False, figure=None, vmin=None, vmax=None,
+                        offset=0):
 
     """
 
@@ -804,6 +810,128 @@ def plot_signal_interp(signal, bvecs, cmap='jet', file_name=None,
 
     
     # Call and return straightaway:
-    return _display_maya(x_plot, y_plot, z_plot, faces, interp_signal,
-                         cmap=cmap, colorbar=colorbar, figure=figure,
-                         vmin=vmin, vmax=vmax, file_name=file_name)
+    return _display_maya_voxel(x_plot, y_plot, z_plot+offset, faces,
+                               interp_signal,
+                               cmap=cmap, colorbar=colorbar, figure=figure,
+                               vmin=vmin, vmax=vmax, file_name=file_name)
+
+
+def plot_cut_planes(vol,
+                    overlay=None,
+                    slice_coronal=None,
+                    slice_saggital=None,
+                    slice_axial=None,
+                    outline=False,
+                    cmap='gray',
+                    overlay_cmap='jet',
+                    invert_cmap=False,
+                    vmin=None,
+                    vmax=None,
+                    figure=None,
+                    view_azim=45.0,
+                    view_elev=45.0):
+    """
+    Display cut planes into a volume
+
+    Parameters
+    ----------
+    vol: 3D array
+
+    overlay: 3D array, optional
+        This will be laid on top of the volume as a second layer (you might
+        want to have a lot of nan's in there...).
+
+    n_planes: int, optional
+       How many planes to show (default: 2)
+
+    outline: bool, optional
+       Whether to add a box outline for each plane.
+
+    cmap: str, optional
+       The name of a mayavi colormap to use (default: 'gray')
+    """ 
+    if not have_maya:
+        e_s = "You can't use this function, unless you have mayavi installed" 
+        raise ValueError(e_s)
+
+    if figure is None:
+        # Adding the engine to a figure will subsequently give you control
+        # through the mayavi GUI:
+        engine = EM.new_engine()
+        figure = maya.figure(engine)    
+    else:
+        figure, engine = figure
+
+    if len(engine.scenes)==0:
+        engine.new_scene()
+
+    # Count yer slices as an indication for how many planes are needed: 
+    n_planes = len(np.where(np.array([slice_coronal,
+                                      slice_axial,
+                                      slice_saggital]))[0])
+    
+    planes = []
+    translator = dict(x_axes = slice_saggital,
+                      y_axes = slice_coronal,
+                      z_axes = slice_axial)
+
+    oris = ['x_axes', 'y_axes', 'z_axes']
+    for i in range(n_planes):
+        # So that you can return all of them: 
+        planes.append(
+            maya.pipeline.image_plane_widget(
+                maya.pipeline.scalar_field(vol),
+                plane_orientation=oris[i],
+                slice_index=translator[oris[i]],
+                transparent=True,
+                colormap=cmap
+                )
+                )
+
+    # We'll want to set an alpha of 0 for nans if those exist: 
+    nans_exist = False
+    if overlay is not None:
+        if np.any(np.isnan(overlay)):
+            nans_exist = True
+            
+        overlay_planes = []
+        for i in range(n_planes):
+            overlay_planes.append(maya.pipeline.image_plane_widget(
+                maya.pipeline.scalar_field(overlay),
+                plane_orientation=oris[i],
+                slice_index=translator[oris[i]],
+                colormap=overlay_cmap,
+                vmin=vmin,
+                vmax=vmax))
+
+    if outline: 
+        maya.outline()
+
+    scene = engine.scenes[0]
+    scene.scene.background = (0.7529411764705882,
+                              0.7529411764705882,
+                              0.7529411764705882)
+
+    # If there are nan values in there, we want to make sure that they get an
+    # alpha value of 0:
+    for child in scene.children[n_planes:]:
+        module_manager = child.children[0]
+        module_manager.scalar_lut_manager.reverse_lut = invert_cmap
+        if nans_exist:
+            lut = module_manager.scalar_lut_manager.lut.table.to_array()
+            lut[0, -1] = 0
+            module_manager.scalar_lut_manager.lut.table = lut
+
+    module_manager.scalar_lut_manager.show_scalar_bar = True
+    module_manager.scalar_lut_manager.show_legend = True
+    module_manager.scalar_lut_manager.scalar_bar.number_of_labels = 5
+    module_manager.scalar_lut_manager.number_of_labels = 5
+    module_manager.scalar_lut_manager.scalar_bar.title = ''
+
+    scene.scene.camera.clipping_range = [170, 480]
+    scene.scene.camera.position = [297, 150, 190]
+
+    maya.view(view_azim, view_elev)
+
+    return figure, engine
+    
