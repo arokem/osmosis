BF: Very subtle bug in applying a rotation to tensor evecs.
In some particular cases, this would null out the secondary evecs. For example for the case:

evals = [1.5, 0.5, 0.5]
evecs = [[1,0,0],[0,0,1],[0,1,0]]

osmosis/tensor.py
@@ -257,7 +257,7 @@ def rotate_to_vector(vector, evals, evecs, bvecs, bvals):
     
     """
     e1 = evecs[0]
-    rot_tensor_e = evecs * ozu.calculate_rotation(vector, e1)
+    rot_tensor_e = np.dot(ozu.calculate_rotation(vector, e1), evecs)
     return tensor_from_eigs(rot_tensor_e, evals, bvecs, bvals)
 
     