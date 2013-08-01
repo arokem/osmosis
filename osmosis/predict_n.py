6a422af1ba1f0f1844805fbb196c08ce79361cf1
Kimberly Chan <kimberlylchan@gmail.com>
2013-08-01 16:24:37 -0700

    Added some more functions and their tests for predict_n.

--- osmosis/predict_n.py
+++ osmosis/predict_n.py
@@ -1,31 +1,56 @@
+"""
+
+This module contains various functions associated with leave n out cross-validation.
+All functions use the multi_bvals' and/or SFM's predict function.
+
+"""
+import numpy as np
+from math import factorial as f
+import itertools
+import time
+
 import osmosis.model.sparse_deconvolution as sfm
 import osmosis.snr as snr
 import osmosis.multi_bvals as sfm_mb
-import numpy as np
-import time
 
-# Predict for all b values
 def predict_n(data, bvals, bvecs, mask, n, b_mode):
     """
-    Predict like before but for 10% instead of each one.
+    Predicts signals for a certain percentage of the vertices.
     
     Parameters
     ----------
-    
-    data: 
-    
-    ...
-       
+    data: 4 dimensional array
+        Diffusion MRI data
+    bvals: 1 dimensional array
+        All b values
+    bvecs: 3 dimensional array
+        All the b vectors
+    mask: 3 dimensional array
+        Brain mask of the data
+    n: int
+        Integer indicating the percent of vertices that you want to predict
+    b_mode: str
+        'all': if fitting to all b values
+        'bvals': if fitting to individual b values
+        
+    Returns
+    -------
+    actual: 2 dimensional array
+        Actual signals for the predicted vertices
+    predicted: 2 dimensional array 
+        Predicted signals for the vertices left out of the fit
     """ 
     t1 = time.time()
     bval_list, b_inds, unique_b, rounded_bvals = snr.separate_bvals(bvals)
     _, b_inds_rm0, unique_b_rm0, rounded_bvals_rm0 = snr.separate_bvals(bvals,
                                                                         mode = 'remove0')
-    all_b_idx = np.squeeze(np.where(rounded_bvals != 0))
+    all_b_idx = np.squeeze(np.where(rounded_bvals != 0))    
     
     actual = np.empty((np.sum(mask), len(all_b_idx)))
     predicted = np.empty(actual.shape)
     
+    # Generate the regressors in the full model from which we choose the regressors in
+    # the reduced model from.  This is so you won't have 
     if b_mode == 'all': 
         full_mod = sfm_mb.SparseDeconvolutionModelMultiB(data, bvecs, bvals,
                                                         mask = mask,
@@ -47,12 +72,15 @@ def predict_n(data, bvals, bvecs, mask, n, b_mode):
         np.random.shuffle(vec_pool)
         
         # How many of the indices are you going to leave out at a time?
-        num_choose = (n/100.)*len(these_b_inds)
+        num_choose = (n/100.)*len(these_b_inds) # np.ceil((n/100.)*len(these_b_inds))
                 
         for combo_num in np.arange(np.floor(100./n)):
             these_inc0 = list(all_inc_0)
             idx = list(b_inds_rm0[bi])
-            vec_pool_inds = vec_pool[(combo_num)*num_choose:(combo_num*num_choose + num_choose)]
+            low = (combo_num)*num_choose
+            high = np.min([(combo_num*num_choose + num_choose), len(vec_pool)])
+            vec_pool_inds = vec_pool[low:high]
+            #vec_pool_inds = vec_pool[(combo_num)*num_choose:(combo_num*num_choose + num_choose)]
             vec_combo = these_b_inds[vec_pool_inds]
             vec_combo_rm0 = these_b_inds_rm0[vec_pool_inds]
                
@@ -73,50 +101,221 @@ def predict_n(data, bvals, bvecs, mask, n, b_mode):
             these_bvals = bvals_pool[these_inc0]
             this_data = data[:, :, :, these_inc0]
             
-            # Need to sort the indices first before indexing full_mod.regressors
+            # Need to sort the indices first before indexing full model's regressors
             si = sorted(idx)
             
             if b_mode is "all":
                 mod = sfm_mb.SparseDeconvolutionModelMultiB(this_data, these_bvecs, these_bvals,
                                                             mask = mask, params_file = "temp")
-                                                            
-                mod.regressors = [full_mod.regressors[0][:, si],
-                                  full_mod.regressors[1][:, si][si, :],
-                                  full_mod.regressors[2][:, si][si, :],
-                                  full_mod.regressors[3][:, si],
-                                  full_mod.regressors[4][:, si]]
-                                  
+                # Grab regressors from full model's preloaded regressors
+                fit_to = full_mod.regressors[0][:, si]
+                tensor_regressor = full_mod.regressors[1][:, si][si, :]
+                mod.regressors = demean(fit_to, tensor_regressor, mod)
+                            
                 predicted[:, vec_combo_rm0] = mod.predict(bvecs[:, vec_combo], bvals[vec_combo])
-                
+
             elif b_mode is "bvals":
                 mod = sfm.SparseDeconvolutionModel(this_data, these_bvecs, these_bvals,
                                                     mask = mask, params_file = "temp")
                                        
                 predicted[:, vec_combo_rm0] = mod.predict(bvecs[:, vec_combo])[mod.mask]
-                
             actual[:, vec_combo_rm0] = data[mod.mask][:, vec_combo]
             
         t2 = time.time()
-        print "This program took %4.2f minutes to run through %3.2f percent of the program."%((t2 - t1)/60,
+        print "This program took %4.2f minutes to run through %3.2f percent."%((t2 - t1)/60,
                                                                 100*((bi+1)/len(unique_b[1:])))
     return actual, predicted
+    
+def demean(fit_to, tensor_regressor, mod):
+    """
+    This function demeans the signals and tensor regressors.
+    
+    Parameters
+    ----------
+    fit_to: 2 dimensional array
+        Original signal fitted to.  Size should be equal to the number of voxels by the
+        number of directions.
+    tensor_regressor: 2 dimensional array
+        The predicted signals from the tensor model.  Size should be equal to the number
+        of directions fit to by the number of directions fit to.
+        
+    Returns
+    -------
+    fit_to: 2 dimensional array
+        Original signal fitted to.  Size should be equal to the number of voxels by the
+        number of directions.
+    tensor_regressor: 2 dimensional array
+        The predicted signals from the tensor model.  Size should be equal to the number
+        of directions fit to by the number of directions fit to.
+    design_matrix: 2 dimensional array
+        Demeaned tensor regressors
+    fit_to_demeaned: 2 dimensional array
+        Demeaned signal fitted to
+    fit_to_means:
+        The means of the original signal fitted to.
+    """
+    
+    fit_to_demeaned = np.empty(fit_to.shape)
+    fit_to_means = np.empty(fit_to.shape)
+    design_matrix = np.empty(tensor_regressor.shape)
+    
+    for bidx, b in enumerate(mod.unique_b):
+        for vox in xrange(mod._n_vox):
+            # Need to demean everything across the vertices that were fitted to
+            fit_to_demeaned[vox, mod.b_inds_rm0[bidx]] = (fit_to[vox, mod.b_inds_rm0[bidx]]
+                                                - np.mean(fit_to[vox, mod.b_inds_rm0[bidx]]))
+            fit_to_means[vox, mod.b_inds_rm0[bidx]] = np.mean(fit_to[vox, mod.b_inds_rm0[bidx]])
+            design_matrix[mod.b_inds_rm0[bidx]] = (tensor_regressor[mod.b_inds_rm0[bidx]]
+                                    - np.mean(tensor_regressor[mod.b_inds_rm0[bidx]].T, -1))
+                                    
+    return [fit_to, tensor_regressor, design_matrix, fit_to_demeaned, fit_to_means]   
     
 def predict_bvals(data, bvals, bvecs, mask, b_fit_to, b_predict):
     """
     Predict for each b value.
+    
+    Parameters
+    ----------
+    data: 4 dimensional array
+        Diffusion MRI data
+    bvals: 1 dimensional array
+        All b values
+    bvecs: 3 dimensional array
+        All the b vectors
+    mask: 3 dimensional array
+        Brain mask of the data
+    b_fit_to: int
+        Unique b value index of the b value to fit to.
+    b_predict: int
+        Unique b value index of the b value to predict.
+        
+    Returns
+    -------
+    actual: 2 dimensional array
+        Actual signals for the predicted vertices
+    predicted: 2 dimensional array 
+        Predicted signals for the vertices left out of the fit
     """
     
     bval_list, b_inds, unique_b, rounded_bvals = snr.separate_bvals(bvals)
     bval_list_rm0, b_inds_rm0, unique_b_rm0, rounded_bvals_rm0 = snr.separate_bvals(bvals,
-                                                                        mode = 'remove0')
-    all_b_idx = np.squeeze(np.where(rounded_bvals != 0))
+                                                                         mode = 'remove0')
     all_inc_0 = np.concatenate((b_inds[0], b_inds[1:][b_fit_to]))
         
     mod = sfm.SparseDeconvolutionModel(data[:,:,:,all_inc_0], bvecs[:,all_inc_0],
                                                rounded_bvals[all_inc_0], mask = mask,
-                                               params_file = 'temp')
-    actual = data[mod.mask][0, b_inds[b_predict]]
-    this_pred = mod.predict(bvecs[:, b_inds[b_predict]])[mod.mask][0]
-    predicted = this_pred
+                                                                params_file = 'temp')
+    actual = data[mod.mask][0, b_inds[1:][b_predict]]
+    predicted = mod.predict(bvecs[:, b_inds[1:][b_predict]])[mod.mask][0]
         
     return actual, predicted
+
+def nchoosek(n,k):
+    """
+    Finds all the number of unique combinations from choosing groups of k from a pool of n.
+    
+    Parameters
+    ----------
+    n: int
+        Number of items in the pool you are choosing from
+    k: int
+        Size of the groups you are choosing from the pool
+        
+    n!/(k!*(n-k)!)
+    """
+    return f(n)/f(k)/f(n-k)
+    
+def choose_AD_RD(AD_start, AD_end, RD_start, RD_end, AD_num, RD_num):
+    """
+    Parameters
+    ----------
+    AD_start: int
+        Lowest axial diffusivity desired
+    AD_end: int
+        Highest axial diffusivity desired
+    RD_start: int
+        Lowest radial diffusivity desired
+    RD_end: int
+        Highest radial diffusivity desired
+    AD_num: int
+        Number of different axial diffusivities
+    RD_num: int
+        Number of different radial diffusivities
+        
+    Returns
+    -------
+    AD_combos: obj
+        Unique axial diffusivity combinations
+    RD_combos: obj
+        Unique radial diffusivity combinations
+    """
+    
+    AD_bag = np.linspace(AD_start, AD_end, num = AD_num)
+    RD_bag = np.linspace(RD_start, RD_end, num = RD_num)
+
+    AD_combos = list(itertools.combinations(AD_bag, 3))
+    RD_combos = list(itertools.combinations(RD_bag, 3))
+    
+    return AD_combos, RD_combos
+    
+def predict_RD_AD(AD_start, AD_end, RD_start, RD_end, AD_num, RD_num, data, bvals, bvecs, mask):
+    """
+    Predicts vertices with different axial and radial diffusivities and finds them
+    root-mean-square error (rmse) between the actual values and predicted values.
+    
+    Parameters
+    ----------
+    AD_start: int
+        Lowest axial diffusivity desired
+    AD_end: int
+        Highest axial diffusivity desired
+    RD_start: int
+        Lowest radial diffusivity desired
+    RD_end: int
+        Highest radial diffusivity desired
+    AD_num: int
+        Number of different axial diffusivities
+    RD_num: int
+        Number of different radial diffusivities
+    data: 4 dimensional array
+        Diffusion MRI data
+    bvals: 1 dimensional array
+        All b values
+    bvecs: 3 dimensional array
+        All the b vectors
+    mask: 3 dimensional array
+        Brain mask of the data
+        
+    Returns
+    -------
+    rmse_b: 1 dimensional array
+        The rmse from fitting to individual b values
+    rmse_mb: 1 dimensional array
+        The rmse from fitting to all the b values
+    AD_order: list
+        The groups of axial diffusivities in the order they were chosen
+    RD_order: list
+        The groups of radial diffusivities in the order they were chosen
+    """
+    AD_combos, RD_combos = choose_AD_RD(AD_start, AD_end, RD_start, RD_end, AD_num, RD_num)
+    
+    AD_order = []
+    RD_order = []
+    rmse_b = np.empty((np.sum(mask), nchoosek(AD_num,3)*nchoosek(RD_num,3)))
+    rmse_mb = np.empty(rmse_b.shape)
+
+    track = 0
+    for AD_idx in np.arange(len(AD_combos)):
+        for RD_idx in np.arange(len(RD_combos)):
+            actual_b, predicted_b = predict_n(data, bvals, bvecs, mask, np.array(AD_combos[AD_idx]), np.array(RD_combos[RD_idx]), 'bvals')
+            actual, predicted = predict_n(data, bvals, bvecs, mask, np.array(AD_combos[AD_idx]), np.array(RD_combos[RD_idx]), 'all')
+            
+            rmse_b[:, track] = np.sqrt(np.mean((actual_b - predicted_b)**2, -1))
+            rmse_mb[:, track] = np.sqrt(np.mean((actual - predicted)**2, -1))
+            
+            track = track + 1
+            
+            AD_order.append(AD_combos[AD_idx])
+            RD_order.append(RD_combos[RD_idx])
+            
+    return rmse_b, rmse_mb, AD_order, RD_order
---
+++ osmosis/tests/test_predict_n.py
@@ -0,0 +1,130 @@
+import numpy as np
+import numpy.testing as npt
+import nibabel as nib
+import os
+
+import osmosis.multi_bvals as sfm_mb
+import osmosis.model.sparse_deconvolution as sfm
+import osmosis.predict_n as pn
+
+# Mock b value array to be used in most tests
+bvals_t = np.array([0.005, 0.005, 0.010, 2.010, 1.005, 0.950, 1.950, 1.000, 1.005,
+                    0.995, 2.005,2.010, 1.995])
+
+bval_list_t = [(np.array([0, 0, 0]))]
+bval_list_t.append(np.array([1000, 1000, 1000, 1000, 1000]))
+bval_list_t.append(np.array([2000, 2000, 2000, 2000, 2000]))
+
+bval_ind_t = [np.array([0,1,2]), np.array([4,5,7,8,9]), np.array([3,6,10,11,12])]
+bval_ind_rm0_t = [np.array([1,2,4,5,6]), np.array([0,3,7,8,9])]
+
+bvals_scaled_t = np.array([0, 0, 0, 2000, 1000, 1000, 2000, 1000, 1000, 1000,
+                           2000, 2000, 2000])
+
+unique_b_t = np.array([0,1,2])
+
+bvecs_t = np.zeros([3,13])
+bvecs_t[0,:] = np.array([1,0,0,-1,0,0, 1/np.sqrt(3), -1/np.sqrt(3), 3/np.sqrt(14),
+                         1/np.sqrt(26), 2/np.sqrt(12), -2/np.sqrt(12),
+                         3/np.sqrt(27)])
+bvecs_t[1,:] = np.array([0,1,0,0,-1,0, 1/np.sqrt(3), -1/np.sqrt(3), 2/np.sqrt(14),
+                        -3/np.sqrt(26), 2/np.sqrt(12), -2/np.sqrt(12),
+                         3/np.sqrt(27)])
+bvecs_t[2,:] = np.array([0,0,1,0,0,-1, 1/np.sqrt(3), -1/np.sqrt(3), 1/np.sqrt(14),
+                         4/np.sqrt(26), 2/np.sqrt(12), -2/np.sqrt(12),
+                         3/np.sqrt(27)])
+
+# Mock data to be used in most tests
+data_t = np.zeros([2,2,2,13])
+data_t[:,:,:,0:3] = 2000 + abs(np.random.randn(2,2,2,3)*500)# For b=0
+data_t[:,:,:,3] = np.squeeze(500 + abs(np.random.randn(2,2,2,1)*200)) # For b=2
+data_t[:,:,:,4:6] = 1000 + abs(np.random.randn(2,2,2,2)*500) # For b=1
+data_t[:,:,:,6] = np.squeeze(500 + abs(np.random.randn(2,2,2,1)*200)) # For b=2
+data_t[:,:,:,7:10] = np.squeeze(1000 + abs(np.random.randn(2,2,2,3)*500))#For b=1
+data_t[:,:,:,10:13] = np.squeeze(500 + abs(np.random.randn(2,2,2,3)*200))#For b=2
+
+# Mock mask to be used in most tests
+mask_t = np.zeros([2,2,2])
+mask_t[:,:,1] = 1
+
+def test_regressors():
+    full_mod_t = sfm_mb.SparseDeconvolutionModelMultiB(data_t, bvecs_t, bvals_t,
+                                                                  mask = mask_t,
+                                                           params_file = "temp")
+    vec_pool_t = np.arange(len(bval_ind_t[1]))
+    np.random.shuffle(vec_pool_t)
+    vec_pool_inds = vec_pool_t[0:3]
+    vec_combo_t = bval_ind_t[1][vec_pool_inds]
+    idx = list(bval_ind_rm0_t[0])
+
+    these_inc0 = list(np.arange(len(bvals_scaled_t)))
+    for choice_idx in vec_pool_inds:
+        these_inc0.remove(bval_ind_t[1][choice_idx])
+        idx.remove(bval_ind_rm0_t[0][choice_idx])
+
+    idx = np.concatenate((idx, bval_ind_rm0_t[1]),0)
+    si_t = sorted(idx)
+    these_inc0 = np.array(these_inc0)
+
+    these_bvecs_t = bvecs_t[:, these_inc0]
+    these_bvals_t = bvals_t[these_inc0]
+    this_data_t = data_t[:, :, :, these_inc0]
+
+    mod = pn.sfm_mb.SparseDeconvolutionModelMultiB(this_data_t, these_bvecs_t,
+                                                 these_bvals_t, mask = mask_t,
+                                                         params_file = "temp")
+
+    these_regressors_t = [full_mod_t.regressors[0][:, si_t],
+                        full_mod_t.regressors[1][:, si_t][si_t, :]]
+                        
+    # Check to see if the regressors indexed from the full model equal the
+    # regressors from the reduced model.
+    npt.assert_equal(mod.regressors[0], these_regressors_t[0])
+    npt.assert_equal(mod.regressors[1], these_regressors_t[1])
+    
+def test_all_predict():
+    # Now check to see if the values for multi b values is around a
+    # value we want
+    actual_vals, predicted_vals = pn.predict_n(data_t, bvals_t, bvecs_t, mask_t,
+                                                                      40, "all")
+    npt.assert_equal(np.mean(predicted_vals[1][predicted_vals[1] >1])>800,1)
+    npt.assert_equal(np.mean(actual_vals[1][actual_vals[1] >1])>800,1)
+
+def test_bval_predict():
+    # Now check to see if the values for individual b values is
+    # around a value we want
+    actual_vals, predicted_vals = pn.predict_n(data_t, bvals_t, bvecs_t, mask_t,
+                                                                    20, "bvals")
+    npt.assert_equal(np.mean(predicted_vals[1][predicted_vals[1] >1])>800,1)
+    npt.assert_equal(np.mean(actual_vals[1][actual_vals[1] >1])>800,1)
+    
+def test_predict_bvals():
+    data_path = "/biac4/wandell/data/klchan13/100307/Diffusion/data"
+    data_pv = nib.load(os.path.join(data_path, "red_data.nii.gz")).get_data()
+    bvals_pv = np.loadtxt(os.path.join(data_path, "bvals"))
+    bvecs_pv = np.loadtxt(os.path.join(data_path, "bvecs"))
+    mask_orig = nib.load(os.path.join(data_path, 'nodif_brain_mask.nii.gz')).get_data()
+    mask_pv = np.zeros(mask_orig.shape)
+    mask_pv[0, 0, 0:2] = 1
+
+    actual02, predicted02 = pn.predict_bvals(data_pv, bvals_pv/1000, bvecs_pv,
+                                                        mask_pv, 0, 2)
+    actual12, predicted12 = pn.predict_bvals(data_pv, bvals_pv/1000, bvecs_pv,
+                                                        mask_pv, 1, 2)
+    actual22, predicted22 = pn.predict_bvals(data_pv, bvals_pv/1000, bvecs_pv,
+                                                        mask_pv, 2, 2)
+
+    rmse02 = np.sqrt(np.mean((actual02 - predicted02)**2))
+    rmse12 = np.sqrt(np.mean((actual12 - predicted12)**2))
+    rmse22 = np.sqrt(np.mean((actual22 - predicted22)**2))
+
+    npt.assert_equal(rmse02>rmse12, 1)
+    npt.assert_equal(rmse12>rmse22, 1)
+    
+def test_predict_RD_AD():
+    rmse_b, rmse_mb, AD_order, RD_order = pn.predict_RD_AD(1.3, 2.0, 0.5, 0.9,
+                                                                 4, 4, data_t,
+                                                             bvals_t, bvecs_t,
+                                                                       mask_t)
+    npt.assert_equal(np.abs(rmse_b[0,1] - rmse_mb[0,1])<100, 1)
+    npt.assert_equal(rmse_mb < 400, 1)
