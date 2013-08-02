fc626f5a500db714132deac0062f13a015cedafe
Kimberly Chan <kimberlylchan@gmail.com>
2013-08-02 15:23:34 -0700

    Added function to put data from parallel predict scripts back together again.

--- osmosis/predict_n.py
+++ osmosis/predict_n.py
@@ -8,10 +8,14 @@ import numpy as np
 from math import factorial as f
 import itertools
 import time
+import os
+import nibabel as nib
+import osmosis.utils as ozu
 
 import osmosis.model.sparse_deconvolution as sfm
 import osmosis.snr as snr
 import osmosis.multi_bvals as sfm_mb
+import osmosis.snr as snr
 
 def predict_n(data, bvals, bvecs, mask, n, b_mode):
     """
@@ -319,3 +323,64 @@ def predict_RD_AD(AD_start, AD_end, RD_start, RD_end, AD_num, RD_num, data, bval
             RD_order.append(RD_combos[RD_idx])
             
     return rmse_b, rmse_mb, AD_order, RD_order
+    
+def place_predict(files):
+    data_path = "/biac4/wandell/data/klchan13/100307/Diffusion/data"
+    file_path = "/hsgs/nobackup/klchan13"
+
+    # Get file object
+    data_file = nib.load(os.path.join(data_path, "data.nii.gz"))
+    wm_data_file = nib.load(os.path.join(data_path,"wm_mask_registered.nii.gz"))
+
+    # Get data and indices
+    wm_data = wm_data_file.get_data()
+    wm_idx = np.where(wm_data==1)
+
+    # b values
+    bvals = np.loadtxt(os.path.join(data_path, "bvals"))
+    bval_list, b_inds, unique_b, rounded_bvals = snr.separate_bvals(bvals/1000)
+    all_b_idx = np.squeeze(np.where(rounded_bvals != 0))
+
+    all_predict_brain = ozu.nans((wm_data_file.shape + all_b_idx.shape))
+    bvals_predict_brain = ozu.nans((wm_data_file.shape + all_b_idx.shape))
+    actual_brain = ozu.nans((wm_data_file.shape + all_b_idx.shape))
+    
+    # Keep track of files in case there are any missing ones
+    i_track = np.ones(1832)
+    for f_idx in np.arange(len(files)):
+        this_file = files[f_idx]
+        if this_file[(len(this_file)-6):len(this_file)] == "nii.gz":
+            sub_data = nib.load(os.path.join(file_path, this_file)).get_data()
+            if this_file[0:11] == "all_predict":
+                i = int(this_file.split(".")[0][11:])
+                print "Placing all_predict %4.2f of 1832"%(i+1)
+                low = i*70
+                high = np.min([(i+1) * 70, int(np.sum(wm_data))])
+                all_predict_brain[wm_idx[0][low:high], wm_idx[1][low:high], wm_idx[2][low:high]] = sub_data
+            elif this_file[0:13] == "bvals_predict":
+                i = int(this_file.split(".")[0][13:])
+                print "Placing bvals_predict %4.2f of 1832"%(i+1)
+                low = i*70
+                high = np.min([(i+1) * 70, int(np.sum(wm_data))])
+                bvals_predict_brain[wm_idx[0][low:high], wm_idx[1][low:high], wm_idx[2][low:high]] = sub_data
+            elif this_file[0:10] == "all_actual":
+                i = int(this_file.split(".")[0][10:])
+                print "Placing actual %4.2f of 1832"%(i+1)
+                low = i*70
+                high = np.min([(i+1) * 70, int(np.sum(wm_data))])
+                actual_brain[wm_idx[0][low:high], wm_idx[1][low:high], wm_idx[2][low:high]] = sub_data
+            i_track[i] = 0
+        
+    missing_files = np.squeeze(np.where(i_track))
+    rmse_b = np.sqrt(np.mean((actual_brain[wm_idx] - bvals_predict_brain[wm_idx])**2,-1))
+    rmse_mb = np.sqrt(np.mean((actual_brain[wm_idx] - all_predict_brain[wm_idx])**2,-1))
+
+    # Save the rmse and predict data
+    aff = data_file.get_affine()
+    nib.Nifti1Image(all_predict_brain, aff).to_filename("all_predict_brain.nii.gz")
+    nib.Nifti1Image(bvals_predict_brain, aff).to_filename("bvals_predict_brain.nii.gz")
+
+    np.save("rmse_b_flat.npy", rmse_b)
+    np.save("rmse_mb_flat.npy", rmse_mb)
+    
+    return missing_files, rmse_b, rmse_mb, all_predict_brain, bvals_predict_brain
