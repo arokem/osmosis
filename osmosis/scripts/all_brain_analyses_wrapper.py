ca0ce5041d7d5d050564ec1debcd6748d05c358b
Kimberly Chan <kimberlylchan@gmail.com>
2014-07-16 23:29:23 -0700

    Uncommenting.

--- osmosis/scripts/all_brain_analyses_wrapper.py
+++ osmosis/scripts/all_brain_analyses_wrapper.py
@@ -26,30 +26,31 @@ port = 22
 
 sid_list = ["103414", "105115", "111312", "113619",
             "115320", "117122", "118730", "118932"] #"110411",
-            
-#def qsub_cmd_gen(template, job_name, i, sid, fODF, im, data_path,
-                 #cmd_file_path=cmd_file_path, python_path=python_path,
-                 #bashcmd=bashcmd, mem=25):
-    #reload(template)
-    #template = sge.getsourcelines(template)[0]
-    
-    #if job_name[0:2] != "im":
-        #params_dict = dict(i=i, sid=sid, fODF=fODF, im=im,
-                        #data_path=data_path)
-        #name = '%s_%s_%s%s'%(job_name,fODF,shorthand_im,i)
-    #else:
-        #params_dict = dict(i=i, sid=sid, im=im,
-                        #data_path=data_path)
-        #name = '%s_%s%s'%(job_name,shorthand_im,i)
-        
-    #code = sge.add_params(template,params_dict)
-    #cmd_file = os.path.join(cmd_file_path, '%s.py'%name)
-    #print("Generating: %s"%cmd_file)
-                        
-    #sge.py_cmd(ssh,
-               #code,
-               #file_name=cmd_file,
-               #python=python_path)
+
+def qsub_cmd_gen(template, job_name, i, sid, fODF, im, data_path,
+                 cmd_file_path=cmd_file_path, python_path=python_path,
+                 bashcmd=bashcmd, mem=25):
+    reload(template)
+    template = sge.getsourcelines(template)[0]
+
+    if job_name[0:2] != "im":
+        params_dict = dict(i=i, sid=sid, fODF=fODF, im=im,
+                        data_path=data_path)
+        name = '%s_%s_%s%s'%(job_name,fODF,shorthand_im,i)
+    else:
+        params_dict = dict(i=i, sid=sid, im=im,
+                        data_path=data_path)
+        name = '%s_%s%s'%(job_name,shorthand_im,i)
+
+    code = sge.add_params(template,params_dict)
+    cmd_file = os.path.join(cmd_file_path, '%s.py'%name)
+    print("Generating: %s"%cmd_file)
+
+    sge.py_cmd(ssh,
+               code,
+               file_name=cmd_file,
+               python=python_path)
+
     cmd_file = os.path.join(cmd_file_path, '%s.py'%name)
     batch_sge.append(sge.qsub_cmd(
         '%s %s'%(bashcmd, cmd_file), name, mem_usage=mem))
@@ -68,12 +69,10 @@ other_file_names = []
 subj_file_nums = []
 for sid_idx, sid in enumerate(sid_list):
     data_path = os.path.join(hcp_path, "%s/T1w/Diffusion"%sid)
-    
-    #wm_data_file = nib.load(os.path.join(data_path,"wm_mask_no_vent.nii.gz"))
-    #wm_vox_num = np.sum(np.round(wm_data_file.get_data()).astype(int))
-    
-    #wm_data_file = nib.load(os.path.join(data_path,"wm_mask_no_vent.nii.gz"))
-    #wm_vox_num = np.sum(wm_data_file.get_data())
+
+    wm_data_file = nib.load(os.path.join(data_path,"wm_mask_no_vent.nii.gz"))
+    wm_vox_num = np.sum(np.round(wm_data_file.get_data()).astype(int))
+
     # For dividing up data
     emd_file_num = int(np.ceil(wm_vox_num/2000.))
     others_file_num = int(np.ceil(wm_vox_num/2000.))
@@ -84,62 +83,177 @@ for sid_idx, sid in enumerate(sid_list):
                 shorthand_im = "be"
             elif im == "single_exp_rs":
                 shorthand_im = "se"
-            ## Reliability
-            #for i in np.arange(emd_file_num):
-                #import osmosis.parallel.emd_template as template
-                #if fODF == "single":
-                    #mem = 35
-                #else:
-                    #mem = 30
-                    
-                #qsub_cmd_gen(template, 'emd_%s'%sid, i, sid, fODF, im,
-                                                   #data_path, mem=mem)
-                #if (sid_idx == 0) & (i == 0):
-                    #emd_file_names.append("emd_%s_%s"%(fODF, shorthand_im))
-            
-            ## Isotropic Model Accuracy
-            ## Only need to calculate isotropic models twice (one for
-            ## each im) per subject
-            #if fODF == "multi":
-                #for i in np.arange(others_file_num):
-                    #import osmosis.parallel.im_accuracy_template as template
-                    #qsub_cmd_gen(template, 'im_cod_%s'%sid, i, sid, fODF, im,
-                                                           #data_path, mem=20)
-                    #if (sid_idx == 0) & (i == 0):
-                        #other_file_names.append("im_cod_%s"%shorthand_im)
-                        #other_file_names.append("im_predict_out_%s"
-                                                     #%shorthand_im)
-                        #other_file_names.append("im_param_out_%s"
-                                                   #%shorthand_im)
-                    
-            ## Diffusion Model Accuracy
-            #for i in np.arange(others_file_num):
-                #import osmosis.parallel.accuracy_template as template
-                #if fODF == "single":
-                    #mem = 35
-                #else:
-                    #mem = 25
-                    
-                #qsub_cmd_gen(template, 'sfm_cod_%s'%sid, i, sid, fODF, im,
-                                                       #data_path, mem=mem)
-                #if (sid_idx == 0) & (i == 0):
-                    #other_file_names.append("sfm_predict_%s_%s"%(fODF,
-                                                        #shorthand_im))
-                    #other_file_names.append("sfm_cod_%s_%s"%(fODF,
-                                                    #shorthand_im))
 
-            ## Model Parameters
-            #for i in np.arange(others_file_num):
-                #import osmosis.parallel.model_params_template as template
-                #qsub_cmd_gen(template, 'sfm_mp_%s'%sid, i, sid, fODF, im,
-                                                       #data_path, mem=25)
-                #if (sid_idx == 0) & (i == 0):
-                    #other_file_names.append("model_params_%s_%s"%(fODF,
-                                                         #shorthand_im))
-        
-## Add some header stuff:
-#batch_sge = ['#!/bin/bash'] + batch_sge
-#sge.write_file_ssh(ssh, batch_sge, batch_sge_file)
+
+            # Reliability
+            for i in np.arange(emd_file_num):
+                import osmosis.parallel.emd_template as template
+                if fODF == "single":
+                    mem = 35
+                else:
+                    mem = 30
+
+                qsub_cmd_gen(template, 'emd_%s'%sid, i, sid, fODF, im,
+                                                   data_path, mem=mem)
+                if (sid_idx == 0) & (i == 0):
+                    emd_file_names.append("emd_%s_%s"%(fODF, shorthand_im))
+
+            # Isotropic Model Accuracy
+            # Only need to calculate isotropic models twice (one for
+            # each im) per subject
+            if fODF == "multi":
+                for i in np.arange(others_file_num):
+                    import osmosis.parallel.im_accuracy_template as template
+                    qsub_cmd_gen(template, 'im_cod_%s'%sid, i, sid, fODF, im,
+                                                           data_path, mem=20)
+                    if (sid_idx == 0) & (i == 0):
+                        other_file_names.append("im_cod_%s"%shorthand_im)
+                        other_file_names.append("im_predict_out_%s"
+                                                     %shorthand_im)
+                        other_file_names.append("im_param_out_%s"
+                                                   %shorthand_im)
+
+            # Diffusion Model Accuracy
+            for i in np.arange(others_file_num):
+                import osmosis.parallel.accuracy_template as template
+                if fODF == "single":
+                    mem = 35
+                else:
+                    mem = 25
+
+                qsub_cmd_gen(template, 'sfm_cod_%s'%sid, i, sid, fODF, im,
+                                                       data_path, mem=mem)
+                if (sid_idx == 0) & (i == 0):
+                    other_file_names.append("sfm_predict_%s_%s"%(fODF,
+                                                        shorthand_im))
+                    other_file_names.append("sfm_cod_%s_%s"%(fODF,
+                                                    shorthand_im))
+
+            # Model Parameters
+            for i in np.arange(others_file_num):
+                import osmosis.parallel.model_params_template as template
+                qsub_cmd_gen(template, 'sfm_mp_%s'%sid, i, sid, fODF, im,
+                                                       data_path, mem=25)
+                if (sid_idx == 0) & (i == 0):
+                    other_file_names.append("model_params_%s_%s"%(fODF,
+                                                         shorthand_im))
+
+# Add some header stuff:
+batch_sge = ['#!/bin/bash'] + batch_sge
+sge.write_file_ssh(ssh, batch_sge, batch_sge_file)
+def qsub_cmd_gen(template, job_name, i, sid, fODF, im, data_path,
+                 cmd_file_path=cmd_file_path, python_path=python_path,
+                 bashcmd=bashcmd, mem=25):
+    reload(template)
+    template = sge.getsourcelines(template)[0]
+
+    if job_name[0:2] != "im":
+        params_dict = dict(i=i, sid=sid, fODF=fODF, im=im,
+                        data_path=data_path)
+        name = '%s_%s_%s%s'%(job_name,fODF,shorthand_im,i)
+    else:
+        params_dict = dict(i=i, sid=sid, im=im,
+                        data_path=data_path)
+        name = '%s_%s%s'%(job_name,shorthand_im,i)
+
+    code = sge.add_params(template,params_dict)
+    cmd_file = os.path.join(cmd_file_path, '%s.py'%name)
+    print("Generating: %s"%cmd_file)
+
+    sge.py_cmd(ssh,
+               code,
+               file_name=cmd_file,
+               python=python_path)
+
+    cmd_file = os.path.join(cmd_file_path, '%s.py'%name)
+    batch_sge.append(sge.qsub_cmd(
+        '%s %s'%(bashcmd, cmd_file), name, mem_usage=mem))
+
+# Analyses done:
+# Reliability, isotropic model accuracy, diffusion model accuracy,
+# fitted model parameters
+ssh = sge.SSH(hostname=hostname,username=username, port=port)
+
+batch_sge = []
+# For aggregating later:
+emd_file_names = []
+other_file_names = []
+
+# Start qsub generation:
+subj_file_nums = []
+for sid_idx, sid in enumerate(sid_list):
+    data_path = os.path.join(hcp_path, "%s/T1w/Diffusion"%sid)
+
+    wm_data_file = nib.load(os.path.join(data_path,"wm_mask_no_vent.nii.gz"))
+    wm_vox_num = np.sum(np.round(wm_data_file.get_data()).astype(int))
+
+    # For dividing up data
+    emd_file_num = int(np.ceil(wm_vox_num/2000.))
+    others_file_num = int(np.ceil(wm_vox_num/2000.))
+    subj_file_nums.append(np.array((emd_file_num, others_file_num)))
+    for fODF in ["multi", "single"]:
+        for im in ["bi_exp_rs", "single_exp_rs"]:
+            if im == "bi_exp_rs":
+                shorthand_im = "be"
+            elif im == "single_exp_rs":
+                shorthand_im = "se"
+
+            # Reliability
+            for i in np.arange(emd_file_num):
+                import osmosis.parallel.emd_template as template
+                if fODF == "single":
+                    mem = 35
+                else:
+                    mem = 30
+
+                qsub_cmd_gen(template, 'emd_%s'%sid, i, sid, fODF, im,
+                                                   data_path, mem=mem)
+                if (sid_idx == 0) & (i == 0):
+                    emd_file_names.append("emd_%s_%s"%(fODF, shorthand_im))
+
+            # Isotropic Model Accuracy
+            # Only need to calculate isotropic models twice (one for
+            # each im) per subject
+            if fODF == "multi":
+                for i in np.arange(others_file_num):
+                    import osmosis.parallel.im_accuracy_template as template
+                    qsub_cmd_gen(template, 'im_cod_%s'%sid, i, sid, fODF, im,
+                                                           data_path, mem=20)
+                    if (sid_idx == 0) & (i == 0):
+                        other_file_names.append("im_cod_%s"%shorthand_im)
+                        other_file_names.append("im_predict_out_%s"
+                                                     %shorthand_im)
+                        other_file_names.append("im_param_out_%s"
+                                                   %shorthand_im)
+
+            # Diffusion Model Accuracy
+            for i in np.arange(others_file_num):
+                import osmosis.parallel.accuracy_template as template
+                if fODF == "single":
+                    mem = 35
+                else:
+                    mem = 25
+
+                qsub_cmd_gen(template, 'sfm_cod_%s'%sid, i, sid, fODF, im,
+                                                       data_path, mem=mem)
+                if (sid_idx == 0) & (i == 0):
+                    other_file_names.append("sfm_predict_%s_%s"%(fODF,
+                                                        shorthand_im))
+                    other_file_names.append("sfm_cod_%s_%s"%(fODF,
+                                                    shorthand_im))
+
+            # Model Parameters
+            for i in np.arange(others_file_num):
+                import osmosis.parallel.model_params_template as template
+                qsub_cmd_gen(template, 'sfm_mp_%s'%sid, i, sid, fODF, im,
+                                                       data_path, mem=25)
+                if (sid_idx == 0) & (i == 0):
+                    other_file_names.append("model_params_%s_%s"%(fODF,
+                                                         shorthand_im))
+
+# Add some header stuff:
+batch_sge = ['#!/bin/bash'] + batch_sge
+sge.write_file_ssh(ssh, batch_sge, batch_sge_file)
 
 # Read batch_sge.sh and get the individual qsub commands.
 batch_sge = sp.check_output(["cat", "batch_sge.sh"])
@@ -150,7 +264,7 @@ cmd_line_split = batch_sge.split('\n')
 red_cmd_line = []
 for cmd_idx in np.arange(1, len(cmd_line_split) - 1):
     pycmd = cmd_line_split[cmd_idx].split(' ')[2].split('_')
-    
+
     if pycmd[0] == "im":
         sid_idx = len(pycmd) - 2
     else:
@@ -181,7 +295,7 @@ if count < max_jobs:
     these_max_jobs = count
 else:
     these_max_jobs = int(max_jobs+1)
-    
+
 for qsub_idx in np.arange(these_max_jobs):
     cmd_arr = np.array(red_cmd_line[qsub_idx].split(' '))
     sp.call(list(cmd_arr[np.where(cmd_arr != '')]))
@@ -195,7 +309,7 @@ while cur_submit_num < count:
         time.sleep(10)
         cur_job_num = len(str(sp.check_output(["qstat", "-u",
                                 "%s"%username])).split('\n'))
-    
+
     # If the number of jobs is less than the max number, submit a few more
     # until the max number of jobs is queued on the cluster
     num_to_submit = int(max_jobs) - cur_job_num
@@ -205,7 +319,7 @@ while cur_submit_num < count:
         cur_submit_num = cur_submit_num + 1
         cmd_arr = np.array(red_cmd_line[qsub_idx].split(' '))
         sp.call(list(cmd_arr[np.where(cmd_arr != '')]))
-        
+
     cur_job_num = len(str(sp.check_output(["qstat", "-u",
                             "%s"%username])).split('\n'))
 
@@ -215,14 +329,14 @@ job_status = sp.check_output(["qstat", "-u", "%s"%username])
 while '%s'%username in job_status:
     time.sleep(10)
     job_status = sp.check_output(["qstat", "-u", "%s"%username])
-    
+
 # Now that the jobs are done, aggregate files and reorganize.
 for sid_idx, sid in enumerate(sid_list):
     data_path = os.path.join(hcp_path, "%s/T1w/Diffusion"%sid)
     os.chdir(data_path)
     wm_data_file = nib.load("wm_mask_no_vent.nii.gz")
     wm_data = np.round(wm_data_file.get_data()).astype(int)
-    
+
     # Grab the number of files total for this subject
     emd_fnum = subj_file_nums[sid_idx][0]
     other_fnum = subj_file_nums[sid_idx][1]
@@ -242,13 +356,13 @@ for sid_idx, sid in enumerate(sid_list):
     for fname_idx, fname in enumerate(other_file_names):
         str_to_write = (str_to_write + "%s is missing files:%s\n"%(fname,
                                                missing_files[fname_idx]))
-    
+
     missing_files_txt = open("missing_files_%s.txt"%sid, "w")
     missing_files_txt.write("%s"%str_to_write)
-    
+
     sp.call(['mkdir', 'file_pieces'])
     sp.call(['mkdir', 'analysis_results'])
-    
+
     params = "mv aggre_* analysis_results | mv *.txt analysis_results "
     params = params + "| mv *.npy file_pieces"
 
